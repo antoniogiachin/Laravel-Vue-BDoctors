aae891ff926bbd0b2014b3f7014f75eb97d388cf
Matteo Morganti <matteo99morganti@gmail.com>
2022-05-02 17:08:50 +0200

    migration

---
+++ database/migrations/2022_05_02_141600_create_leads_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLeadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leads', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('doctor_id')->nullable();
+            $table->foreignId('doctor_id')->constrained();
+            $table->string('author');
+            $table->string('email');
+            $table->text('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leads');
+    }
+}
