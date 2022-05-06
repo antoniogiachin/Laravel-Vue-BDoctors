1b440f505ecf100e1f287c4a055c33c59fc42d77
Matteo Morganti <matteo99morganti@gmail.com>
2022-05-06 14:04:59 +0200

    aggiunta pages single doctor

---
+++ resources/js/pages/SingleDoctor.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="container">
+        <h1>{{doctor.name}}</h1>
+
+    <form @submit.prevent="sendForm">
+        <!-- Inserire nome utente -->
+        <div class="form-group">
+            <label for="author">nome e cognome</label>
+            <input type="text" class="form-control" :class="{'is-invalid':errors.author}" id="author" name="author" v-model="author">
+            <p v-for="(error, index) in errors.author" :key="'error_author'+index" class="invalid-feedback">
+                {{error}}
+            </p>
+        </div>
+
+        <!-- Inserire email utente -->
+        <div class="form-group">
+            <label for="email">Inserisci la tua email</label>
+            <input type="email" class="form-control" :class="{'is-invalid':errors.email}" id="email" name="email" v-model="email">
+            <p v-for="(error, index) in errors.email" :key="'error_email'+index" class="invalid-feedback">
+                {{error}}
+            </p>
+        </div>
+
+        <!-- Motivo del contatto -->
+        <div class="form-group">
+            <label for="message">Motivo del contatto</label>
+            <textarea class="form-control" :class="{'is-invalid':errors.message}" id="message" rows="6" name="message" v-model="message"></textarea>
+            <p v-for="(error, index) in errors.message" :key="'error_message'+index" class="invalid-feedback">
+                {{error}}
+            </p>
+        </div>
+
+         <button type="submit" class="btn btn-primary">{{sendingInProgress? 'Invio in corso...':'Invia'}}</button>
+    </form>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SingleDoctor",
+    data() {
+        return {
+            author: '',
+            email: '',
+            message: '',
+            sendingInProgress: false,
+            errors: {},
+            success: false,
+            doctor: null
+        }
+    },
+    methods: {
+        sendForm() {
+            this.sendingInProgress = true;
+            axios.post('/api/single-doctor', {
+                'author': this.author,
+                'email' : this.email,
+                'message': this.message,
+            }).then(res => {
+                this.sendingInProgress = false;
+
+                if(res.data.errors) {
+                    this.errors = res.data.errors;
+                    this.success = false;
+                } else {
+                    this.succcess = true;
+                    this.author = '',
+                    this.email= '',
+                    this.errors = {};
+                }
+            });
+        }
+    },
+    mounted() {
+        const slug = this.$route.params.slug;
+
+        axios.get('/api/doctors/' + slug).then(res => {
+            if(res.data.success == false) {
+                abort(404, 'not found');
+            } else {
+                this.doctor = res.data.results;
+            }
+        })
+    }
+}
+</script>
+
+<style>
+
+</style>
