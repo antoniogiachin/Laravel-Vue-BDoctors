49c8ecce8931cebc74ef9076caaaf46931f8330d
Flavio Rodolfi <flek8rodolfi@gmail.com>
2022-05-10 10:23:51 +0200

    config

--- public/js/front.js
+++ public/js/front.js
@@ -21238,7 +21238,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/antoniogiachin/Desktop/laravel-vue-BDoctors/resources/js/front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\Users\flek8\OneDrive\Desktop\Personal Works\laravel-vue-BDoctors\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
