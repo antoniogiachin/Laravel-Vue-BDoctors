c6a233af1be3b735545c1705ac891a13bbe3c9b6
Matteo Morganti <matteo99morganti@gmail.com>
2022-05-11 16:48:58 +0200

    modificata dashboard - login_bg - register_bg

--- resources/views/Admin/home.blade.php
+++ resources/views/Admin/home.blade.php
@@ -1,70 +1,99 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row">
-    <div class="col-6 mx-auto">
-        @if (session('alreadyCreated'))
-            <div class="alert alert-danger" role="alert">
-                {{ session('alreadyCreated') }}
-            </div>
-        @endif
-        @if (session('status'))
-            <div class="alert alert-success" role="alert">
-                {{ session('status') }}
-            </div>
-        @endif
-    </div>
-</div>
-<div class="container mt-4">
-    <div class="row justify-content-center">
-        <div>
-            <div class="row">
-                <div class="col-12">
-                    <p class="text-center text-uppercase h4">Dashboard</p>
+
+@php
+  $user = Auth::user();
+  $name = $user->name;
+  $surname = $user->surname;
+  $email = $user->email;
+  $address = $user->address;
+@endphp
+
+    <div class="row">
+        <div class="col-6 mx-auto">
+            @if (session('alreadyCreated'))
+                <div class="alert alert-danger" role="alert">
+                    {{ session('alreadyCreated') }}
                 </div>
-
-                <div class="col-12">
-                    <div class="row">
-                        @if (!$doctor)
-                            <p class="text-center mt-3">Completa subito il tuo profilo!</p>
-                            {{-- creazione profilo dottore --}}
-                            <div class="col-6 d-flex justify-content-center">
-                                <a href="{{ route('admin.doctors.create') }}" class="btn mt-3 text-white btn-success">Registra il tuo profilo da dottore</a>
-                            </div>
-                            {{-- eliminazione user --}}
-                            <div class="col-6 d-flex justify-content-center">
-                                <form action="{{ route('admin.home.destroy', $user->id) }}" id="deleteUser" method="POST">
-                                    @csrf
-
-                                    @method('DELETE')
-
-                                    <button class=" btn btn-danger text-white mt-3" type="submit">Elimina il tuo profilo</button>
-                                </form>
-                            </div>
-                        @else
-                            <div class="col-4 p-3">
-                                <p>Bentornato signore/a  {{ $doctor->user->name }}  {{ $doctor->user->surname }}</p>
-                            </div>
-                            <div class="col-8 d-flex align-items-center justify-content-start gap-2">
-                                {{-- show --}}
-                                <a class="btn text-dark btn-primary" href="{{ route('admin.doctors.show', $doctor->slug) }}">Visualizza il tuo profilo da dottore</a>
-                                {{-- edit --}}
-                                <a class="btn text-dark btn-warning" href="{{ route('admin.doctors.edit', $doctor->slug) }}">Modifica il tuo profilo da dottore</a>
-                                {{-- delete --}}
-                                <form action="{{ route('admin.doctors.destroy', $doctor->id) }}" method="POST" class="delete-profile" data-name="{{$doctor->user->surname}}">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button class="btn btn-danger">Elimina il tuo profilo da dottore</button>
-                                </form>
-                            </div>
-                        @endif
-
-                    </div>
-
-
+            @endif
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
                 </div>
-            </div>
+            @endif
         </div>
     </div>
-</div>
+    <div class="container mt-4 bg-light d-flex justify-content-around border">
+        {{-- sidebar info dottore --}}
+        <div class="d-flex flex-column flex-shrink-0 p-3 bg-info border-end me-3" style="width: 280px;">
+            @if (!$doctor->photo)
+                <img src=" {{ asset('img/not_found.jpg') }} " alt="not_found_photo" class="img-fluid py-2">
+            @else
+                <img src=" {{ asset('storage/' . $doctor->photo) }} " alt="{{ $doctor->id }}_photo" class="img-fluid py-2">
+            @endif
+            <h5>{{$name}} {{$surname}}</h5>
+            <div class="div d-flex flex-wrap mt-2">
+                @foreach ($doctor->specialties as $specialty)
+                    <span class="badge bg-light text-dark m-1">{{ $specialty->name }}</span>
+                @endforeach
+            </div>
+           
+            <hr>
+            <ul class="nav nav-pills flex-column mb-auto">
+                <li class="nav-item">
+                    <p><i class="fa-solid fa-location-pin"></i>
+                        @if(!$doctor->medical_address)
+                            <span>Studio non presente</span>
+                        @else
+                            <span> {{$doctor->medical_address}}</span>
+                        @endif
+                    </p>
+                </li>
+                <li>
+                    <p><i class="fa-solid fa-envelope"></i> {{$email}}</p>
+                </li>
+                <li>
+                    <p><i class="fa-solid fa-phone"></i>
+                        @if(!$doctor->cv)
+                            <span>telefono non presente</span>
+                        @else
+                           <span>{{$doctor->phone}}</span>
+                        @endif
+                    </p>
+                </li>
+                <li>
+                    <p><i class="fa-solid fa-file"></i>
+                        @if(!$doctor->cv)
+                            <span>Nessun cv</span>
+                        @else
+                            <a class="nav-link text-dark d-inline" href="{{url('storage/'. Auth::user()->doctor->cv)}}">Guarda Cv</a>
+                        @endif
+                    </p>
+                </li>
+            </ul>
+        </div>
+        <div class="row text-center justify-content-center mt-3">
+            <div class="col-md-6 col-sm-12 d-flex justify-content-center align-items-center ms_hov_active">
+                <h4><i class="fa-solid fa-gears"></i><a class="nav-link text-dark d-inline" href="{{route('admin.doctors.edit', $doctor->slug)}}">Modifica Profilo</a></h4>
+            </div>
+            <div class="col-md-6 col-sm-12 d-flex justify-content-center align-items-center ms_hov_active">
+               <h4><i class="fa-solid fa-message"></i><a class="nav-link text-dark d-inline" href="#">Messaggi ricevuti</a></h4>
+            </div>
+            <div class="col-md-6 col-sm-12 d-flex justify-content-center align-items-center ms_hov_active">
+               <h4><i class="fa-solid fa-comments"></i><a class="nav-link text-dark d-inline" href="#">Recensioni ricevute</a></h4>
+            </div>
+            <div class="col-md-6 col-sm-12 d-flex justify-content-center align-items-center ms_hov_active">
+               <h4><i class="fa-solid fa-chart-line"></i><a class="nav-link text-dark d-inline" href="#">Statistiche</a></h4>
+            </div>
+            <div class="col-md-6 col-sm-12 d-flex justify-content-center align-items-center ms_hov_active">
+               <h4><i class="fa-solid fa-circle-plus"></i><a class="nav-link text-dark d-inline" href="#">Aggiungi Sponsor</a></h4>
+            </div>
+            <div class="col-md-6 col-sm-2 d-flex justify-content-center align-items-center ms_hov_active">
+                {{-- Da aggiungere condizione: se il dott. ha sponsor attivi mostra "sponsor attivi", altrimenti mostra "nessuno sponsor attivo" --}}
+               <h4><i class="fa-solid fa-stamp"></i><a class="nav-link text-dark d-inline" href="#">Nessuno sponsor attivo</a></h4>
+            </div>
+        </div>
+
+    </div>
 @endsection
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,76 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-5 py-4">
-            <div class="card">
-                <div class="card-header text-center text-uppercase bg-white">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="mb-3 d-flex flex-column align-items-center">
-
-                            <div class="col-md-6">
-                                <label for="email" class="form-label">{{ __('E-mail') }}</label>
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="mb-3 flex-column align-items-center d-flex ">
-
-                            <div class="col-md-6">
-                                <label for="password" class="form-label">{{ __('Password') }}</label>
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="mb-3 flex-column align-items-center d-flex ">
-                            <div class="col-md-6">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Ricordami') }}
-                                    </label>
+<div id="bg-doctor-login">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-5 py-4">
+                <div class="card shadow-lg">
+                    <div class="card-header text-center text-uppercase bg-white">{{ __('Login') }}</div>
+    
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+    
+                            <div class="mb-3 d-flex flex-column align-items-center">
+    
+                                <div class="col-md-6">
+                                    <label for="email" class="form-label">{{ __('E-mail') }}</label>
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+    
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
                                 </div>
                             </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="d-flex  justify-content-center mt-3">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Hai dimenticato la tua password?') }}
-                                    </a>
-                                @endif
+    
+                            <div class="mb-3 flex-column align-items-center d-flex ">
+    
+                                <div class="col-md-6">
+                                    <label for="password" class="form-label">{{ __('Password') }}</label>
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+    
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
                             </div>
-                        </div>
-                    </form>
+    
+                            <div class="mb-3 flex-column align-items-center d-flex ">
+                                <div class="col-md-6">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+    
+                                        <label class="form-check-label" for="remember">
+                                            {{ __('Ricordami') }}
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+    
+                            <div class="form-group row mb-0">
+                                <div class="d-flex  justify-content-center mt-3">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Login') }}
+                                    </button>
+
+                                    @if (Route::has('password.request'))
+                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                            {{ __('Hai dimenticato la tua password?') }}
+                                        </a>
+                                    @endif
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
 @endsection
